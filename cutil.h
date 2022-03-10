08992421ed74ecca75786c42b0f42d37448b2395
Kosuke Futamata <kousukehutamata@gmail.com>
2022-03-11 05:15:20 +0900

    Initial commit

---
+++ cutil.h
@@ -0,0 +1,26 @@
+#ifndef CUTIL_H
+#define CUTIL_H
+
+// list
+// linked-list
+// stack
+// queue
+// priority-queue
+// string-builder
+// hash-set
+// sorted-set
+// hash-map
+// sorted-map
+// union-find-tree
+// segment-tree
+// fenwick-tree
+
+// iterator
+
+// sort
+// select
+// where
+
+// shared ptr, unique ptr
+
+#endif // CUTIL_H
